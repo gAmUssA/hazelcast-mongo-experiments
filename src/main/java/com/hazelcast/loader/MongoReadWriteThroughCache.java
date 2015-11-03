4d47c9ad65b2da196b80f80869ddeb0cb6f27c3e
Viktor Gamov <gamussa@gmail.com>
2015-11-02 20:39:18 -0500

    initial commit of mongo integration

---
+++ src/main/java/com/hazelcast/loader/MongoReadWriteThroughCache.java
@@ -0,0 +1,14 @@
+package com.hazelcast.loader;
+
+/**
+ * TODO
+ *
+ * @author Viktor Gamov on 11/2/15.
+ *         Twitter: @gamussa
+ * @since 0.0.1
+ */
+public class MongoReadWriteThroughCache {
+    public static void main(String[] args) {
+
+    }
+}
